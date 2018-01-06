78619fca933d2b4e857c82a7a99491ff3ce10714
Luan Nico <luanpotter27@gmail.com>
2018-01-06 14:40:57 -0200

    Fix firebase auth

--- server/src/main/java/xyz/luan/monstros/ws/FirebaseAuthFilter.java
+++ server/src/main/java/xyz/luan/monstros/ws/FirebaseAuthFilter.java
@@ -14,85 +14,89 @@ import xyz.luan.monstros.domain.user.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class FirebaseAuthFilter extends HttpFilter {
 
-	private Logger LOGGER = LoggerFactory.getLogger(FirebaseAuthFilter.class);
+    private Logger LOGGER = LoggerFactory.getLogger(FirebaseAuthFilter.class);
 
-	@Override
-	protected void filter(HttpServletRequest request, HttpServletResponse response) throws ExecutionException, InterruptedException {
-		String authToken = request.getHeader("Authorization");
-		LOGGER.info("Request received [url: %s][auth: %s]", request.getRequestURI(), authToken);
+    @Override
+    protected void filter(HttpServletRequest request, HttpServletResponse response) throws IOException, ExecutionException, InterruptedException {
+        String authToken = request.getHeader("Authorization");
+        LOGGER.info("Request received [url: %s][auth: %s]", request.getRequestURI(), authToken);
 
-		if (request.getMethod().equals("OPTIONS")) {
-			return;
-		}
+        if (request.getMethod().equals("OPTIONS")) {
+            return;
+        }
 
-		if (Objects.isNull(authToken) || !authToken.contains(" ")) {
-			throw new HttpException(403, "Login is mandatory for every API request, add a 'Authorization: Bearer <token>' header with firebase auth");
-		}
+        if (Objects.isNull(authToken) || !authToken.contains(" ")) {
+            throw new HttpException(403, "Login is mandatory for every API request, add a 'Authorization: Bearer <token>' header with firebase auth");
+        }
 
-		String idToken = authToken.split(" ")[1];
-		FirebaseToken decodedToken = getFirebase().verifyIdTokenAsync(idToken).get();
-		if (!decodedToken.isEmailVerified()) {
-			throw new HttpException(403, "You can only login with a verified e-mail!");
-		}
+        String idToken = authToken.split(" ")[1];
+        FirebaseToken decodedToken = getFirebase().verifyIdTokenAsync(idToken).get();
+        if (!decodedToken.isEmailVerified()) {
+            throw new HttpException(403, "You can only login with a verified e-mail!");
+        }
 
-		String email = decodedToken.getEmail();
-		String[] parts = email.split("@");
-		if (parts.length != 2) {
-			throw new HttpException(422, "You can only login with an email that contains exactly one '@' character (sorry, I know that spec allows weird stuff, but we can't handle it)");
-		}
+        String email = decodedToken.getEmail();
+        String[] parts = email.split("@");
+        if (parts.length != 2) {
+            throw new HttpException(422, "You can only login with an email that contains exactly one '@' character (sorry, I know that spec allows weird stuff, but we can't handle it)");
+        }
 
-		String domain = parts[1];
-		Company company = getOrCreateCompany(domain);
-		User user = getOrCreateUser(email, company);
+        String domain = parts[1];
+        Company company = getOrCreateCompany(domain);
+        User user = getOrCreateUser(email, company);
 
-		AuthHolder.company.set(company);
-		AuthHolder.user.set(user);
-	}
+        AuthHolder.company.set(company);
+        AuthHolder.user.set(user);
+    }
 
-	private User getOrCreateUser(String email, Company company) {
-		User user = Yawp.yawp(User.class).where("company", "=", company.getId()).and("email", "=", email).first();
-		if (user == null) {
-			user = createNewUser(email, company);
-		}
-		return user;
-	}
+    private User getOrCreateUser(String email, Company company) {
+        User user = Yawp.yawp(User.class).where("company", "=", company.getId()).and("email", "=", email).first();
+        if (user == null) {
+            user = createNewUser(email, company);
+        }
+        return user;
+    }
 
-	private User createNewUser(String email, Company company) {
-		User user;
-		User newUser = new User();
-		newUser.setCompany(company.getId());
-		newUser.setEmail(email);
-		user = Yawp.yawp.save(newUser);
-		return user;
-	}
+    private User createNewUser(String email, Company company) {
+        User user;
+        User newUser = new User();
+        newUser.setCompany(company.getId());
+        newUser.setEmail(email);
+        user = Yawp.yawp.save(newUser);
+        return user;
+    }
 
-	private Company getOrCreateCompany(String domain) {
-		Company c = Yawp.yawp(Company.class).where("domain", "=", domain).first();
-		if (c != null) {
-			return c;
-		}
-		return createNewCompany(domain);
-	}
+    private Company getOrCreateCompany(String domain) {
+        Company c = Yawp.yawp(Company.class).where("domain", "=", domain).first();
+        if (c != null) {
+            return c;
+        }
+        return createNewCompany(domain);
+    }
 
-	private Company createNewCompany(String domain) {
-		Company c2 = new Company();
-		c2.setDomain(domain);
-		return Yawp.yawp.save(c2);
-	}
+    private Company createNewCompany(String domain) {
+        Company c2 = new Company();
+        c2.setDomain(domain);
+        return Yawp.yawp.save(c2);
+    }
 
-	private static FirebaseAuth _auth;
+    private static FirebaseAuth _auth;
 
-	private static FirebaseAuth getFirebase() {
-		if (_auth == null) {
-			FirebaseOptions options = new FirebaseOptions.Builder().setProjectId("cartas-oposicao").setCredentials(GoogleCredentials.newBuilder().build()).build();
-			FirebaseApp app = FirebaseApp.initializeApp(options);
-			_auth = FirebaseAuth.getInstance(app);
-		}
-		return _auth;
-	}
+    private static FirebaseAuth getFirebase() throws IOException {
+        if (_auth == null) {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setProjectId("cartas-oposicao")
+                    .setCredentials(GoogleCredentials.getApplicationDefault())
+                    .build();
+            FirebaseApp app = FirebaseApp.initializeApp(options);
+            _auth = FirebaseAuth.getInstance(app);
+        }
+        return _auth;
+    }
 }
