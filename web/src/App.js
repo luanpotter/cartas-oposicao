09a94e42895624d5ca06f413ef2fb996ef462072
Luan Nico <luanpotter27@gmail.com>
2018-01-06 12:12:11 -0200

    Adding basic firebase login

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCERrddd8CzLd3axsLfEYhq0xiiflmQsvI",
+  authDomain: "cartas-oposicao.firebaseapp.com",
+  databaseURL: "https://cartas-oposicao.firebaseio.com",
+  projectId: "cartas-oposicao",
+  storageBucket: "cartas-oposicao.appspot.com",
+  messagingSenderId: "153053898759"
+});
+
+const getLogin = () => {
+  const data = localStorage.getItem("login_data");
+  if (data) {
+    return JSON.parse(data);
+  }
+  return null;
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: getLogin()
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gerador de Cartas de Oposição</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.login ? this.state.login.token : this.loginPage()}
         </p>
       </div>
     );
+  }
+
+  loginPage() {
+    return <button onClick={() => this.doLogin()}>Login</button>;
+  }
+
+  doLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+      const token = result.credential.accessToken;
+      const login = { token };
+      localStorage.setItem('login_data', JSON.stringify(login));
+      this.setState({ login });
+    }).catch(error => console.log(JSON.stringify(error)));
   }
 }
 
